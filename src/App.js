54e60aba6861972c67a9f2ac366142aa45060954
Divianshu Jaswal <divianshu1@gmail.com>
2023-02-05 05:27:23 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Chat from './components/Chat'
+import Login from './components/Login'
+import Register from './components/Register'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css';
+import SetAvatar from './components/SetAvatar'
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <ToastContainer />
+            <Routes>
+                <Route exact path='/register' element={<Register />} />
+                <Route exact path='/login' element={<Login />} />
+                <Route exact path='/' element={<Chat />} />
+                <Route exact path='/setAvatar' element={<SetAvatar />} />
+
+            </Routes>
+        </BrowserRouter>
+    )
 }
 
-export default App;
+export default App
