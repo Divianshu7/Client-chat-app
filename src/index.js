07fc62e867043b3e68410e12f5c55dafa4d941f5
Divianshu Jaswal <divianshu1@gmail.com>
2023-02-08 19:26:42 +0530

    deploy

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import App from './App'
 import './index.css'
 import { createRoot } from 'react-dom/client'
 // ReactDOM.render(<App />, )
+import { disableReactDevTools } from '@fvilers/disable-react-devtools'
+if (process.env.NODE_ENV === 'production') disableReactDevTools()
 const node = document.getElementById('root')
 const root = createRoot(node)
 root.render(<App />)
